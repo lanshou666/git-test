61e8c6580d350a737c2e1aa247dcf700de8bebb5
lanshou <lanshou@12.com>
2022-07-16 19:42:02 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName: GitTest
+ * @Description:
+ * @Author: lanshou666
+ * @Create: 2022-07-16 19:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
